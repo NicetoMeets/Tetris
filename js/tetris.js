50f182111d5b39a6a39470a0a25254cb93e24ff9
Nice <puma1800@naver.com>
2022-11-02 14:17:00 +0900

    키 방향키이동

--- js/tetris.js
+++ js/tetris.js
@@ -12,8 +12,8 @@ let downInterval;
 let tempMovingItem;
 
 const BLOCKS = {
-    tree : [
-        [[0,0],[0,1],[1,0],[1,1]],
+    tree: [
+        [[0, 0], [0, 1], [1, 0], [1, 1]],
         [],
         [],
         [],
@@ -21,7 +21,7 @@ const BLOCKS = {
 }
 
 const movingItem = {
-    type:"tree",
+    type: "tree",
     direction: 0,
     top: 0,
     left: 0,
@@ -31,19 +31,19 @@ const movingItem = {
 init()
 
 // functions
-function init(){
-    tempMovingItem = {...movingItem};
-    for (let i=0; i < GAME_ROWS; i++){
+function init() {
+    tempMovingItem = { ...movingItem };
+    for (let i = 0; i < GAME_ROWS; i++) {
         prependNewLine()
     }
     renderBlocks()
 }
 
 
-function prependNewLine(){
+function prependNewLine() {
     const li = document.createElement("li");
     const ul = document.createElement("ul");
-    for(let j=0; j < GAME_COLS; j++){
+    for (let j = 0; j < GAME_COLS; j++) {
         const matrix = document.createElement("li");
         ul.prepend(matrix);
     }
@@ -51,13 +51,55 @@ function prependNewLine(){
     playground.prepend(li)
 }
 
-function renderBlocks(){
-    const { type, direction, top, left } = tempMovingItem;
 
-    BLOCKS[type][direction].forEach(block=>{
-        const x = block[0];
-        const y = block[1];
+function renderBlocks() {
+    const { type, direction, top, left } = tempMovingItem;
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving=> {
+        moving.classList.remove(type, "moving");
+    })
+
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0] + left;
+        const y = block[1] + top;
         const target = playground.childNodes[y].childNodes[0].childNodes[x];
-        target.classList.add(type)
+        target.classList.add(type, "moving")
     })
 }
+
+// moveBlock
+function moveBlock(moveType, amount) {
+    tempMovingItem[moveType] += amount;
+    renderBlocks();
+}
+
+function changeDirection() {
+    const currDirection = tempMovingItem.direction;
+    currDirection === 3
+        ? (tempMovingItem.direction = 0)
+        : (tempMovingItem.direction += 1);
+
+    renderBlocks();
+}
+
+
+//event handling
+document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+        //각각의 키는 고유의 keycode를 가짐
+        case 40: // top
+            moveBlock("top", 1);
+            break;
+        case 39: // right
+            moveBlock("left", 1);
+            break;
+        case 37: // left
+            moveBlock("left", -1);
+            break;
+        case 38: // up
+            changeDirection();
+            break;
+        default:
+            break;
+    }
+});
